6506a8fa6d17ea6ff70463f1e2c98d475aed730e
alejandro <131113028+EPTalejandro@users.noreply.github.com>
2026-06-07 19:07:31 -0400

    Actualizacion: se ha modificado schema.sql para añadir el embeding, modificar los pk por uuid y otros pequeños detalles

--- squema.sql
+++ squema.sql
@@ -1,5 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+drop table if exists ubicacion cascade;
+drop table if exists camara cascade;
+drop table if exists evento cascade;
+drop table if exists alerta cascade;
+drop table if exists objeto cascade ;
+
 create table ubicacion(
-	UID SERIAL primary key ,
+	UID UUID DEFAULT gen_random_uuid() PRIMARY KEY ,
 	edificio varchar(100),
 	piso smallint not null,
 	zonetype varchar(50) not null,
@@ -7,18 +14,18 @@ create table ubicacion(
 	longitud decimal(10,7) not null 
 );
 
-create table camaras(
-	CID varchar(20) primary key,
+create table camara(
+	CID varchar(20) PRIMARY KEY,
 	modelo varchar(100) not null,
-	UID integer,
+	UID UUID,
 	has_night_vision boolean not null DEFAULT false,
 	estado varchar(20) not null default 'activa',
 	constraint UID foreign key(UID) references ubicacion
 );
 
 create table evento(
-		EID SERIAL primary key,
-		eTiempo timestampz default current_timestamp, 
+		EID UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+		eTiempo timestamptz default current_timestamp, 
 		conf_level decimal(4,3) not null,
 		posX integer not null,
 		posY integer not null,
@@ -29,21 +36,23 @@ create table evento(
 );
 
 create table alerta(
-	AID SERIAL primary key,
-	EID integer,
+	AID UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	EID UUID,
 	constraint EID foreign key(EID) references evento,
-	aTiempo timestampz default current_timestamp,
+	aTiempo timestamptz default current_timestamp,
 	severidad varchar(10) not null,
 	estado varchar(15) default 'pendiente',
 	descripcion TEXT not null 
 );
 
 create table objeto(
-	OID SERIAL primary key,
-	EID integer,
+	OID UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	EID UUID,
 	constraint EID foreign key(EID) references evento,
 	tipo varchar(10) not null,
 	color varchar(50),
 	vehiculo varchar(20),
-	matricula varchar(20)
+	matricula varchar(20),
+	embedding vector(512)
 );
+
