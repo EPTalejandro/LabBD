7f4a908ee48b30640b1f3d9ceb086357914b9dbb
sunnyjoke <carlosbisognob@gmail.com>
2026-07-09 21:10:49 -0400

    función 3 completa

--- funciones.sql
+++ funciones.sql
@@ -37,14 +37,23 @@ create or replace function find_similar_objects(ref_id INT, threshold FLOAT, max
 returns table(object_ID UUID, object_type varchar(10), distancia_coseno vector(512), camera_name varchar(20), tiempo timestamptz)
 begin
 	return query
+	WITH reference_vector as (
+		select em.embedding_vec as v
+		from "object" as o
+		join embedding as em on o.OID = em.OID
+		where ref_id = o.OID
+	)
 	select 
 		o.OID as object_ID,
 		o.object_type, 
 		(em.embedding_vec <=> mv.v) as distancia_coseno
 		c.name as camera_name
-		e.eTime as 
+		e.eTime as tiempo
 	from "object" as o
-	JOIN "event" as e ON o.EID = e.EID
-	JOIN camera as c ON e.CID = c.CID
+	join reference_vector as rv on true
+	join "event" as e on o.EID = e.EID
+	join camera as c on e.CID = c.CID
+	where (em.embedding_vec <=> rv.v) < threshold and o.OID != ref_id
+	limit max_results
 	
 end;
