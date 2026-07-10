5cccb1aae74b6111bb1ff8d65228736b32b475c3
sunnyjoke <carlosbisognob@gmail.com>
2026-07-09 20:01:36 -0400

    inicio función 3

--- funciones.sql
+++ funciones.sql
@@ -21,6 +21,25 @@ begin
 	return query
 	select *
 	from "location" as l 
-	where
+	where 
 end;
 $$ language plpgsql;
+
+-- Retornar una tabla con: id del objeto encontrado, tipo, distancia coseno, nombre de la
+-- cámara donde fue detectado y timestamp del evento.
+
+create or replace function find_similar_objects(ref_id INT, threshold FLOAT, max_results INT)
+returns table(object_ID UUID, object_type varchar(10), distancia_coseno vector(512), camera_name varchar(20), tiempo timestamptz)
+begin
+	return query
+	select 
+		o.OID as object_ID,
+		o.object_type, 
+		(em.embedding_vec <=> mv.v) as distancia_coseno
+		c.name as camera_name
+		e.eTime as 
+	from "object" as o
+	JOIN "event" as e ON o.EID = e.EID
+	JOIN camera as c ON e.CID = c.CID
+	
+end;
