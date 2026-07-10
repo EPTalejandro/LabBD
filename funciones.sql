3e089737334bbc746aaa89485303e2e982fb99df
sunnyjoke <carlosbisognob@gmail.com>
2026-07-09 21:13:30 -0400

    eliminar comentario

--- funciones.sql
+++ funciones.sql
@@ -30,8 +30,7 @@ begin
 end;
 $$ language plpgsql;
 
--- Retornar una tabla con: id del objeto encontrado, tipo, distancia coseno, nombre de la
--- cámara donde fue detectado y timestamp del evento.
+
 
 create or replace function find_similar_objects(ref_id INT, threshold FLOAT, max_results INT)
 returns table(object_ID UUID, object_type varchar(10), distancia_coseno vector(512), camera_name varchar(20), tiempo timestamptz)
