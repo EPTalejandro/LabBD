74dfbe5940bdf1b33bcfc22d3042a3fd193ba16b
alejandro <131113028+EPTalejandro@users.noreply.github.com>
2026-08-02 08:29:25 -0400

    Actulizacion: ya estan creados los tres endpoints analiticos y parte de los endpoints de CRUD, aunque estos no cuentan con verificacion de datos ni manejo de errores

--- endpoints.py
+++ endpoints.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import PlainTextResponse
 import psycopg2 
 from tabulate import tabulate
-from datetime import date
+from datetime import date,datetime,timedelta
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -22,7 +22,7 @@ class Ubicacion(BaseModel):
     latitude: float
     longitud: float
 
-@app.get("/ubicaciones",response_class=PlainTextResponse)
+@app.get("/ubicacion",response_class=PlainTextResponse)
 def mostrar():
     conexion = psycopg2.connect(**config_db)
     cursor = conexion.cursor()
@@ -37,17 +37,17 @@ def mostrar():
     cursor.close()
     conexion.close()
     
-    return tabulate(ubicaciones,headers=headers)
+    return tabulate(eventos,headers=headers)
 
-@app.post("/ubicaciones",response_class=PlainTextResponse)
+@app.post("/ubicacion",response_class=PlainTextResponse)
 def crear(datos: Ubicacion):
     try:
         conexion = psycopg2.connect(**config_db)
         with conexion:
             with conexion.cursor() as cursor:
-                sql_ubicacion = """INSERT INTO "location" ("name", floor, zone_type, latitude, longitude) VALUES (%s, %s, %s, %s, %s) RETURNING UID;"""
+                sql_evento = """INSERT INTO "location" ("name", floor, zone_type, latitude, longitude) VALUES (%s, %s, %s, %s, %s) RETURNING UID;"""
                 parametros = (datos.nombre,datos.piso,datos.tipo_zona,datos.latitude,datos.longitud)
-                cursor.execute(sql_ubicacion,parametros)
+                cursor.execute(sql_evento,parametros)
     except psycopg2.Error as error:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -68,15 +68,15 @@ def mostrar():
     headers = ["CID","Nombre","Modelo","Vision Nocturna","Estado"]
     sql_code = """Select * from camera"""
     cursor.execute(sql_code)
-    ubicaciones = []
+    camaras = []
     for linea in cursor.fetchall():
-        ubicacion = [linea[0],linea[2], linea[3], linea[4],linea[5]]
-        ubicaciones.append(ubicacion)
+        camara = [linea[0],linea[2], linea[3], linea[4],linea[5]]
+        camaras.append(camara)
         
     cursor.close()
     conexion.close()
     
-    return tabulate(ubicaciones,headers=headers)
+    return tabulate(camaras,headers=headers)
 
 @app.get("/eventos",response_class=PlainTextResponse)
 def mostrar():
@@ -85,15 +85,15 @@ def mostrar():
     headers = ["EID","Hora","Nivel de confianza"]
     sql_code = """Select * from event"""
     cursor.execute(sql_code)
-    ubicaciones = []
+    eventos = []
     for linea in cursor.fetchall():
-        ubicacion = [linea[0], linea[2], linea[3]]
-        ubicaciones.append(ubicacion)
+        evento = [linea[0], linea[2], linea[3]]
+        eventos.append(evento)
         
     cursor.close()
     conexion.close()  
         
-    return tabulate(ubicaciones,headers=headers)
+    return tabulate(eventos,headers=headers)
 
 
 @app.get("/analytics/zones/{tipo}",response_class=PlainTextResponse)
@@ -134,4 +134,26 @@ def trafico(id,desde,hasta):
     conexion.close()
     
     return(tabulate(datos,headers=headers))
+
+
+@app.get("/analytics/alerts/summary",response_class=PlainTextResponse)
+def mostrar(days:int):
+    conexion = psycopg2.connect(**config_db)
+    cursor = conexion.cursor()
+    fecha = (datetime.now() - timedelta(days=days),)
+    sql_code = """
+                  Select c.CID,c.name,c.model,c.has_night_vision,c.state,count(a.severity) filter (where a.severity='critica') as alertas_criticas,count(a.severity) filter (where a.severity='alta') as alertas_altas,count(a.severity) filter (where a.severity='media') as alertas_medias,count(a.severity) filter (where a.severity='baja') as alertas_bajas 
+                  from camera as c 
+                  left join event as e on e.CID=c.CID
+                  left join alert as a on a.EID=e.EID AND a.Atime >= %s
+                  group by c.CID 
+               """
+    cursor.execute(sql_code,fecha)
     
+    headers = ["CID","Nombre","Modelo","Vision Nocturna","Estado","alertas_criticas","alertas_altas","alertas_medias","alertas_bajas"]
+    datos = [[f"{dato[0]}",f"{dato[1]}",f"{dato[2]}",f"{dato[3]}",f"{dato[4]}",f"{dato[5]}",f"{dato[6]}",f"{dato[7]}",f"{dato[8]}"] for dato in cursor.fetchall()]
+    
+    cursor.close()
+    conexion.close()
+    
+    return(tabulate(datos,headers=headers))
